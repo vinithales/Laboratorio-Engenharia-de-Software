d672695bdf11bab45b68921880823f5b8170a82f
vinithales <127968652+vinithales@users.noreply.github.com>
2025-02-24 21:39:53 -0300

    ajuste

--- Semana 1/ex4/script.js
+++ Semana 1/ex4/script.js
@@ -1,7 +1,21 @@
 let dado = prompt("Digite um dado :");
 
+// Converte o dado para número (se for possível)
+let dadoConvertido = Number(dado);
+
+// Verifica o tipo do dado
 let VerificarTipo = confirm("Você deseja verificar o tipo do dado informado?");
 
 if (VerificarTipo) {
-    alert("O tipo do dado informado é: " + typeof dado);
-} 
+    if (dado === null) {
+        alert("O tipo do dado informado é: null");
+    } else if (dado === "true" || dado === "false") {
+        alert("O tipo do dado informado é: string (valor booleano em string)");
+    } else if (isNaN(dadoConvertido)) {
+        alert("O tipo do dado informado é: string");
+    } else {
+        alert("O tipo do dado informado é: number");
+    }
+} else {
+    alert("Dado não confirmado");
+}
