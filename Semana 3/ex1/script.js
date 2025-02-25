1a3f2cc47054a3a96d7edcae67b9e997954c322b
vinithales <127968652+vinithales@users.noreply.github.com>
2025-02-24 21:51:45 -0300

    completo

--- Semana 3/ex1/script.js
+++ Semana 3/ex1/script.js
@@ -1,14 +1,18 @@
 document.getElementById('campo').addEventListener('click', ()=>{
     var valor = document.getElementById('valor').value;
-
+    var lista = document.querySelectorAll('ul');
     var valores = new Array();
     
     valores.push(valor);
 
     var valoresOrdenados = valores.sort();
 
-    for (let i = 0; i < array.length; index++) {
-        const element = array[i]
+    for (let i = 0; i < valoresOrdenados.length; i++) {
+        let novoLi = document.createElement("li");
+
+        novoLi.textContent(valoresOrdenados[i]);
+
+        lista.appendChild(novoLi);
     }
 
 });
