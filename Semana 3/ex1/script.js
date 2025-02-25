58f21c8701246d696a2b20445dfbf6bd584066ff
vinithales <127968652+vinithales@users.noreply.github.com>
2025-02-24 21:17:15 -0300

    Semana 3 atividade 1

---
+++ Semana 3/ex1/script.js
@@ -0,0 +1,10 @@
+document.getElementById('campo').addEventListener('click', ()=>{
+    var valor = document.getElementById('valor').value;
+
+    var valores = new Array();
+    
+    valores.push(valor);
+
+    alert(valores);
+
+});
