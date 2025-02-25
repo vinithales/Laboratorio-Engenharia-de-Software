ed997abe50b8825bc0f7b41d8596cc302833876b
vinithales <127968652+vinithales@users.noreply.github.com>
2025-02-24 22:02:12 -0300

    atividade 1 completo

--- Semana 3/ex1/script.js
+++ Semana 3/ex1/script.js
@@ -1,18 +1,21 @@
-document.getElementById('campo').addEventListener('click', ()=>{
+
+let valores = [];
+
+document.getElementById('campo').addEventListener('click', () => {
     var valor = document.getElementById('valor').value;
-    var lista = document.querySelectorAll('ul');
-    var valores = new Array();
-    
+    var lista = document.querySelector('ul'); 
+
     valores.push(valor);
 
     var valoresOrdenados = valores.sort();
 
+    lista.innerHTML = ''; 
+
     for (let i = 0; i < valoresOrdenados.length; i++) {
-        let novoLi = document.createElement("li");
-
-        novoLi.textContent(valoresOrdenados[i]);
-
-        lista.appendChild(novoLi);
+        let novoLi = document.createElement("li"); 
+        novoLi.textContent = valoresOrdenados[i]; 
+        lista.appendChild(novoLi); 
     }
 
+    document.getElementById('valor').value = '';
 });
