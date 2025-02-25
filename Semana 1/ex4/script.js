d3e768d2e6512e3969316fe826a9416969248749
vinithales <127968652+vinithales@users.noreply.github.com>
2025-02-24 21:36:52 -0300

    ajuste

--- Semana 1/ex4/script.js
+++ Semana 1/ex4/script.js
@@ -1,13 +1,7 @@
-
 let dado = prompt("Digite um dado :");
-
 
 let VerificarTipo = confirm("Você deseja verificar o tipo do dado informado?");
 
 if (VerificarTipo) {
-
-    document.body.innerHTML = "<h1>O tipo do dado informado é: " + typeof dado + "</h1>";
-} else {
-    
-    document.write("<h1>Dado não confirmado</h1>");
-}
+    alert("O tipo do dado informado é: " + typeof dado);
+} 
